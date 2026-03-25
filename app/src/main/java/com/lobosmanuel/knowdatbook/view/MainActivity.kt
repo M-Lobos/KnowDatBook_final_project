4369ff7ffa6d042c28cac3cab679f6ac333c3f74
M-Lobos <manuel.lobos.88@gmail.com>
2026-03-24 21:06:35 -0300

    FEAT: first part of the UI builded

--- app/src/main/java/com/lobosmanuel/knowdatbook/view/MainActivity.kt
+++ app/src/main/java/com/lobosmanuel/knowdatbook/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.lobosmanuel.knowdatbook
+package com.lobosmanuel.knowdatbook.view
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.lobosmanuel.knowdatbook.R
 import com.lobosmanuel.knowdatbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/lobosmanuel/knowdatbook/view/SecondFragment.kt
+++ app/src/main/java/com/lobosmanuel/knowdatbook/view/SecondFragment.kt
@@ -1,4 +1,4 @@
-package com.lobosmanuel.knowdatbook
+package com.lobosmanuel.knowdatbook.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.lobosmanuel.knowdatbook.R
 import com.lobosmanuel.knowdatbook.databinding.FragmentSecondBinding
 
 /**
