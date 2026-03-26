75bd8343b506e43f8bae43e9ef2827974852a6e0
M-Lobos <manuel.lobos.88@gmail.com>
2026-03-26 14:20:40 -0300

    MVP FINISHED

--- app/src/main/java/com/lobosmanuel/knowdatbook/view/user/UserSectionFragment.kt
+++ app/src/main/java/com/lobosmanuel/knowdatbook/view/user/UserSectionFragment.kt
@@ -1,9 +1,12 @@
 package com.lobosmanuel.knowdatbook.view.user
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -46,8 +49,27 @@ class UserSectionFragment : Fragment() {
 
                 // Opcional: Toast informativo
                 // Toast.makeText(requireContext(), "¡Libro marcado como leído!", Toast.LENGTH_SHORT).show()
-            }
+
+                }
         )
+
+        binding.btnSugerirLibro.setOnClickListener {
+            println("DEBUG: Click en el botón detectado")
+            Toast.makeText(requireContext(), "Abriendo correo...", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("m.lobos.dev@gmail.com"))
+                putExtra(Intent.EXTRA_SUBJECT, "Sugerencia: KnowDatBook")
+                putExtra(Intent.EXTRA_TEXT, "Hola Manuel, mi sugerencia es: ")
+            }
+
+            try {
+                startActivity(Intent.createChooser(intent, "Selecciona una App"))
+            } catch (e: Exception) {
+                println("DEBUG: Error al lanzar intent: ${e.message}")
+            }
+        }r
 
         binding.rvUserSectionFragment.layoutManager = LinearLayoutManager(requireContext())
         binding.rvUserSectionFragment.adapter = adapter
